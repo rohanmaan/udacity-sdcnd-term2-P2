eb9a44e304e69fd48aab9d6fd436d7f8530de2a3
rohanmaan <rohanmaan62@gmail.com>
2017-10-06 14:13:51 +0530

    First Commit

---
+++ src/tools.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+  TODO:
+    * Calculate the RMSE here.
+  */
+	//cout<<"Calculating the RMSE for the filter....."<<endl;
+	VectorXd rmse(4);
+	rmse<<0,0,0,0;
+	if (estimations.size()!=ground_truth.size() || estimations.size()==0){
+		cout<<"estimations and ground truth data is not valid";
+		return rmse;
+	}
+
+
+	for(unsigned int i=0;i<estimations.size();++i){
+		VectorXd residual = estimations[i]-ground_truth[i];
+		residual=residual.array()*residual.array();
+		rmse = residual +rmse;
+
+	}
+	rmse=rmse/estimations.size();
+	rmse=rmse.array().sqrt();
+	cout<<"RMSE = "<<rmse <<endl;
+	return rmse;
+}
